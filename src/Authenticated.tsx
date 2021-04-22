96ab74a2b45bbf5594cdeb055e8c805097a4ab40
pranavmalvawala <pranavmalvawala@gmail.com>
2021-04-22 10:49:01 +0530

    remove appName related routing and its component

--- src/Authenticated.tsx
+++ src/Authenticated.tsx
@@ -3,7 +3,6 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { Header } from "./components";
 import { ProfilePage } from './profile/ProfilePage';
 import { RolePage } from './churches/RolePage';
-import { AppAccess } from './churches/AppAccess';
 import { ChurchesPage } from './churches/ChurchesPage';
 import { ManageChurch } from './churches/ManageChurch';
 import { ChurchPage } from './churches/ChurchPage';
@@ -32,7 +31,6 @@ export const Authenticated: React.FC<Props> = (props) => {
           <Route path="/admin"><AdminPage /></Route>
           <Route path="/churches/:id/manage" component={ManageChurch} />
           <Route path="/churches/:id/role/:roleId" component={RolePage} />
-          <Route path="/churches/:id/:app" component={AppAccess} />
           <Route path="/churches/:id" component={ChurchPage} />
           <Route path="/churches"><ChurchesPage /></Route>
           <Route path="/profile"><ProfilePage /></Route>
