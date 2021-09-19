6fad4fc13115135b5676aa0704bfb5eb933b5163
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-09-19 22:22:51 +0530

    stop using churchapps for fetching app list (#91)

--- src/churches/ChurchPage.tsx
+++ src/churches/ChurchPage.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Row, Col, Button } from "react-bootstrap"
 import UserContext from "../UserContext";
-import { DisplayBox, ChurchInterface, ApiHelper, UserHelper, EnvironmentHelper, ChurchAppInterface, Permissions, BreadCrumb, BreadCrumbProps } from "./components"
+import { DisplayBox, ChurchInterface, ApiHelper, UserHelper, Permissions, BreadCrumb, BreadCrumbProps } from "./components"
 import { RouteComponentProps, useHistory } from "react-router-dom";
 
 type TParams = { id?: string };
 
 export const ChurchPage = ({ match }: RouteComponentProps<TParams>) => {
   const [church, setChurch] = React.useState<ChurchInterface>(null);
-  const [churchApps, setChurchApps] = React.useState<ChurchAppInterface[]>([]);
   const context = React.useContext(UserContext);
   const history = useHistory();
 
@@ -17,36 +16,30 @@ export const ChurchPage = ({ match }: RouteComponentProps<TParams>) => {
     if (churchId !== UserHelper.currentChurch.id) UserHelper.selectChurch(context, churchId);
 
     ApiHelper.get("/churches/" + match.params.id + "?include=permissions", "AccessApi").then(data => setChurch(data));
-    ApiHelper.get("/churchApps/", "AccessApi").then(data => { setChurchApps(data) });
-
   }
 
   const getAppRows = () => {
     const result: JSX.Element[] = [];
     if (church !== null) {
-            churchApps?.forEach((app, index) => {
-              result.push(<tr key={index}>
-                <td>{app.appName}</td>
-                <td>{getLoginLink(church, app.appName)}</td>
-              </tr>);
-            });
+      // TODO: return user's apps list. List will list all the apps in most cases.
     }
     return result;
   }
 
-  const getLoginLink = (church: ChurchInterface, appName: string) => {
-    const jwt = ApiHelper.getConfig("AccessApi").jwt;
-    let result: JSX.Element = null;
-    switch (appName) {
-      case "CHUMS":
-        result = (<a href={EnvironmentHelper.ChumsUrl + "/login/?jwt=" + jwt + "&churchId=" + church.id.toString()} rel="external noopener noreferrer " target="_blank"><i className="fas fa-external-link-alt"></i></a>);
-        break;
-      case "StreamingLive":
-        result = (<a href={EnvironmentHelper.StreamingLiveUrl.replace("{key}", UserHelper.currentChurch.subDomain) + "/login/?jwt=" + jwt + "&churchId=" + church.id.toString()} rel="external noopener noreferrer" target="_blank"><i className="fas fa-external-link-alt"></i></a>);
-        break;
-    }
-    return result;
-  }
+  // TODO: redirect urls for list of apps
+  // const getLoginLink = (church: ChurchInterface, appName: string) => {
+  //   const jwt = ApiHelper.getConfig("AccessApi").jwt;
+  //   let result: JSX.Element = null;
+  //   switch (appName) {
+  //     case "CHUMS":
+  //       result = (<a href={EnvironmentHelper.ChumsUrl + "/login/?jwt=" + jwt + "&churchId=" + church.id.toString()} rel="external noopener noreferrer " target="_blank"><i className="fas fa-external-link-alt"></i></a>);
+  //       break;
+  //     case "StreamingLive":
+  //       result = (<a href={EnvironmentHelper.StreamingLiveUrl.replace("{key}", UserHelper.currentChurch.subDomain) + "/login/?jwt=" + jwt + "&churchId=" + church.id.toString()} rel="external noopener noreferrer" target="_blank"><i className="fas fa-external-link-alt"></i></a>);
+  //       break;
+  //   }
+  //   return result;
+  // }
 
   const getSidebar = () => {
     if (!UserHelper.checkAccess(Permissions.accessApi.settings.edit) || church === null) return null;
