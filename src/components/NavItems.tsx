7389416268de382157cbb52fabf313316e98203b
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-10-06 08:31:07 +0530

    simplify NavItems component (#100)

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { UserHelper } from "./";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Permissions }  from "../helpers/Permissions"
 
 interface Props {
@@ -16,38 +15,19 @@ interface Tab {
 }
 
 export function NavItems({ prefix }: Props) {
-  const location = useLocation();
-
-  const getSelected = (): string => {
-    let url = location.pathname;
-    let result = "Church";
-    if (url.indexOf("/admin") > -1) result = "Admin";
-    if (url.indexOf("/profile") > -1) result = "Profile";
-
-    return result;
-  };
-
-  const getClass = (sectionName: string): string => {
-    if (sectionName === getSelected()) return prefix === "main" ? "nav-link active" : "active";
-    else return prefix === "main" ? "nav-link" : "";
-  };
-
   const getTab = ({ key, url, icon, label }: Tab) => (
     <li key={key} className="nav-item" data-toggle={prefix === "main" ? null : "collapse"} data-target={prefix === "main" ? null : "#userMenu"} id={(prefix || "") + key + "Tab"}>
-      <Link className={getClass(key)} to={url}>
+      <NavLink className={prefix === "main" ? "nav-link" : ""} to={url}>
         <i className={icon}></i> {label}
-      </Link>
+      </NavLink>
     </li>
   );
 
-  const getTabs = () => {
-    let tabs = [];
-    const churchId = UserHelper.currentChurch.id
-    tabs.push(getTab({ key: "Church", url: `/${churchId}`, icon: "fas fa-church", label: "Church" }));
-    if (UserHelper.checkAccess(Permissions.accessApi.server.admin)) tabs.push(getTab({ key: "Admin", url: "/admin", icon: "fas fa-user-shield", label: "Admin" }));
-    tabs.push(getTab({ key: "Profile", url: "/profile", icon: "fas fa-user", label: "Profile" }));
-    return tabs;
-  };
+  const churchId = UserHelper.currentChurch.id
+  const tabs = []
+  tabs.push(getTab({ key: "Church", url: `/${churchId}`, icon: "fas fa-church", label: "Church" }))
+  if (UserHelper.checkAccess(Permissions.accessApi.server.admin)) tabs.push(getTab({ key: "Admin", url: "/admin", icon: "fas fa-user-shield", label: "Admin" }));
+  tabs.push(getTab({ key: "Profile", url: "/profile", icon: "fas fa-user", label: "Profile" }));
 
-  return <>{getTabs()}</>;
+  return <>{tabs}</>;
 };
